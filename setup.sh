ca58ab0a29fbe051eacff9677a0d22ef0de730fc
angahpadu <81055755+angahpadu@users.noreply.github.com>
2021-10-24 16:16:48 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,17 +11,6 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-IZIN=$( curl https://raw.githubusercontent.com/angahpadu/angahvps/ipbaikbanget | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Please Contact Admin"
-echo "Telegram t.me/djas08"
-echo "WhatsApp wa.me/62822220428038"
-rm -f setup.sh
-exit 0
-fi
 if [ -f "/etc/v2ray/domain" ]; then
 echo "Script Already Installed"
 exit 0
