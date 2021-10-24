dccf3ce4e8d5b334863dca47a22c61b3a749bc3c
angahpadu <81055755+angahpadu@users.noreply.github.com>
2021-10-24 16:13:11 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP=$(wget -qO- icanhazip.com);
 IZIN=$( curl https://raw.githubusercontent.com/angahpadu/angahvps/ipbaikbanget | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
